88ae2e7d97d4704cd89532e9b29bd5ff676e523c
Steve James <0x2t1ff@gmail.com>
2021-12-25 15:55:04 +0000

    try concurrency

--- src/relay.rs
+++ src/relay.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 use std::error::Error;
+use std::sync::{Arc};
+use tokio::sync::Mutex;
 use std::str;
 use std::time::Instant;
 use tokio::net::{TcpListener, TcpStream};
@@ -9,9 +11,7 @@ use tokio_util::compat::{Compat};
 use crate::messages::Message;
 use crate::utils::{start_ws_handshake};
 
-pub struct Relay {
-  rooms: HashMap<String, RoomInfo>,
-}
+pub struct Relay {}
 
 struct RoomInfo {
   sender: ClientInfo,
@@ -28,79 +28,90 @@ struct ClientInfo {
 
 impl Relay {
   pub fn new() -> Self {
-    Relay {
-      rooms: HashMap::new(),
-    }
+    Relay {}
   }
 
   pub async fn start(&mut self) -> Result<(), Box<dyn Error>> {
     println!("Starting Relay Server...");
     let listener = TcpListener::bind("0.0.0.0:8004").await?;
     let mut incoming = TcpListenerStream::new(listener);
+    let rooms = Arc::new(Mutex::new(HashMap::new()));
 
     while let Some(socket) = incoming.next().await {
-      let (mut sender, mut receiver) = start_ws_handshake(socket?).await?;
-
-      let mut data = Vec::new();
-      let _data_type = receiver.receive_data(&mut data).await?;
-      let message: Message = serde_json::from_str(str::from_utf8(&data).unwrap())?;
-
-      match message {
-        Message::Send(message) => {
-          let code = message.code;
-          let filename = message.filename;
-          let size = message.size;
-          let room = RoomInfo {
-            sender: ClientInfo {
-              tx: sender,
-              rx: receiver,
-            },
-            receiver: None,
-            filename,
-            size,
-            opened: Instant::now(),
-          };
-          self.rooms.insert(code.to_string(), room);
-        }
-        Message::Get(message) => {
-          let code = message.code;
-          match self.rooms.get_mut(&code) {
-            Some(room) => {
-              // Send Approval request for this file
-              let approve_req = Message::new_approve_req(room.filename.to_string(), room.size);
-              sender.send_text(approve_req).await?;
-              let mut data = Vec::new();
-              receiver.receive_data(&mut data).await?;
-              let res_message: Message = serde_json::from_slice(&data)?;
-              if let Message::ApproveRes(res) = res_message {
-                if !res.approved {
-                  continue;
-                }
-              }
-
-              // Send Ready message to start the file transfer from the sending client
-              let ready_message = Message::new_ready();
-              room
-                .sender
-                .tx
-                .send_text(serde_json::to_string(&ready_message)?)
-                .await?;
-              let mut data = Vec::new();
-              room.sender.rx.receive_data(&mut data).await?;
-              sender.send_binary(&data).await?;
-              self.rooms.remove(&code).unwrap();
-            }
-            None => {
-              let error_message = Message::new_error(String::from("Room code does not exist"));
-              sender
-                .send_text(serde_json::to_string(&error_message)?)
-                .await?;
-            }
-          };
-        }
-        _ => {}
-      };
+      let rooms = rooms.clone();
+      tokio::spawn(async move {
+        process_req(socket.unwrap(), rooms).await.expect("Error when processing request");
+      });
     }
     Ok(())
   }
+}
+
+async fn process_req(socket: TcpStream, rooms: Arc<Mutex<HashMap<String, RoomInfo>>>) -> Result<(), Box<dyn Error>> {
+    let (mut sender, mut receiver) = start_ws_handshake(socket).await?;
+
+    let mut data = Vec::new();
+    let _data_type = receiver.receive_data(&mut data).await?;
+    let message: Message = serde_json::from_str(str::from_utf8(&data).unwrap())?;
+
+    match message {
+      Message::Send(message) => {
+        let code = message.code;
+        let filename = message.filename;
+        let size = message.size;
+        let room = RoomInfo {
+          sender: ClientInfo {
+            tx: sender,
+            rx: receiver,
+          },
+          receiver: None,
+          filename,
+          size,
+          opened: Instant::now(),
+        };
+        let mut rooms = rooms.lock().await;
+        rooms.insert(code.to_string(), room);
+      },
+
+
+      Message::Get(message) => {
+        let code = message.code;
+        let mut rooms = rooms.lock().await;
+        match rooms.get_mut(&code) {
+          Some(room) => {
+            // Send Approval request for this file
+            let approve_req = Message::new_approve_req(room.filename.to_string(), room.size);
+            sender.send_text(approve_req).await?;
+            let mut data = Vec::new();
+            receiver.receive_data(&mut data).await?;
+            let res_message: Message = serde_json::from_slice(&data)?;
+            if let Message::ApproveRes(res) = res_message {
+              if !res.approved {
+                return Ok(());
+              }
+            }
+
+            // Send Ready message to start the file transfer from the sending client
+            let ready_message = Message::new_ready();
+            room
+              .sender
+              .tx
+              .send_text(serde_json::to_string(&ready_message)?)
+              .await?;
+            let mut data = Vec::new();
+            room.sender.rx.receive_data(&mut data).await?;
+            sender.send_binary(&data).await?;
+            rooms.remove(&code).unwrap();
+          }
+          None => {
+            let error_message = Message::new_error(String::from("Room code does not exist"));
+            sender
+              .send_text(serde_json::to_string(&error_message)?)
+              .await?;
+          }
+        };
+      }
+      _ => {}
+    };
+    Ok(())
 }
