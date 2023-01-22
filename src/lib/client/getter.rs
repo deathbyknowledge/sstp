6f9dc5b37256193628084e1bc4cc2bbba8f4715e
Steve James <sam@flynn.local>
2023-01-22 20:14:29 +0100

    Revert "try at changing mutex for rwlock"
    This reverts commit 4921646621470be38378d2e3c66d1697054fe545.

--- src/lib/client/getter.rs
+++ src/lib/client/getter.rs
@@ -7,74 +7,74 @@ use std::net::SocketAddr;
 use std::str;
 
 pub struct Getter {
-    pub code: String,
-    pub relay_addr: SocketAddr,
-    ws_conn: WSConn,
-    pub filename: Option<String>,
-    pub size: Option<u64>,
-    pub peer_addr: Option<SocketAddr>,
+  pub code: String,
+  pub relay_addr: SocketAddr,
+  ws_conn: WSConn,
+  pub filename: Option<String>,
+  pub size: Option<u64>,
+  pub peer_addr: Option<SocketAddr>,
 }
 
 impl Getter {
-    pub fn new(code: Option<&str>, relay_addr: Option<&str>) -> Self {
-        let code = code.expect("Room code must be provided").to_string();
-        let relay_addr = parse_relay_addr(relay_addr);
-        Getter {
-            code,
-            relay_addr,
-            ws_conn: WSConn::new_empty(),
-            filename: None,
-            size: None,
-            peer_addr: None,
-        }
+  pub fn new(code: Option<&str>, relay_addr: Option<&str>) -> Self {
+    let code = code.expect("Room code must be provided").to_string();
+    let relay_addr = parse_relay_addr(relay_addr);
+    Getter {
+      code,
+      relay_addr,
+      ws_conn: WSConn::new_empty(),
+      filename: None,
+      size: None,
+      peer_addr: None,
     }
+  }
 
-    pub async fn connect(&mut self) -> Result<(), Box<dyn Error>> {
-        let (tx, rx) = start_ws_conn(self.relay_addr).await?;
-        self.ws_conn.init(tx, rx);
+  pub async fn connect(&mut self) -> Result<(), Box<dyn Error>> {
+    let (tx, rx) = start_ws_conn(self.relay_addr).await?;
+    self.ws_conn.init(tx, rx);
+    Ok(())
+  }
+
+  pub async fn get_room(&mut self) -> Result<(), Box<dyn Error>> {
+    let mut msg = Message::new_get(&self.code);
+    self.ws_conn.send(&mut msg).await?;
+
+    let mut data = Vec::new();
+    self.ws_conn.recv(&mut data).await?;
+    let message: Message = serde_json::from_slice(&data)?;
+    match message {
+      Message::Error(error) => Err(Box::new(error)),
+      Message::ApproveReq(req) => {
+        self.filename = Some(req.filename.clone());
+        self.size = Some(req.size);
+        self.peer_addr = Some(req.addr);
         Ok(())
+      }
+      _ => unreachable!(),
     }
+  }
 
-    pub async fn get_room(&mut self) -> Result<(), Box<dyn Error>> {
-        let mut msg = Message::new_get(&self.code);
-        self.ws_conn.send(&mut msg).await?;
+  pub async fn send_approval(&mut self, approved: bool) -> Result<(), Box<dyn Error>> {
+    let mut res_message = Message::new_approve_res(approved);
+    self.ws_conn.send(&mut res_message).await?;
+    Ok(())
+  }
 
-        let mut data = Vec::new();
-        self.ws_conn.recv(&mut data).await?;
-        let message: Message = serde_json::from_slice(&data)?;
-        match message {
-            Message::Error(error) => Err(Box::new(error)),
-            Message::ApproveReq(req) => {
-                self.filename = Some(req.filename.clone());
-                self.size = Some(req.size);
-                self.peer_addr = Some(req.addr);
-                Ok(())
-            }
-            _ => unreachable!(),
-        }
+  pub async fn start_transfer(
+    &mut self,
+    wtr: &mut impl Write,
+    f: impl Fn(u64) -> (),
+  ) -> Result<(), Box<dyn Error>> {
+    let chunks = calc_chunks(self.size.unwrap());
+    let mut data = Vec::with_capacity(1_000_000);
+    for _ in 0..chunks {
+      self.ws_conn.recv(&mut data).await?;
+      let mut msg: ContentMessage = serde_json::from_slice(&data)?;
+      f(msg.content.len().try_into()?);
+      wtr.write(&mut msg.content)?;
+      data.clear();
     }
-
-    pub async fn send_approval(&mut self, approved: bool) -> Result<(), Box<dyn Error>> {
-        let mut res_message = Message::new_approve_res(approved);
-        self.ws_conn.send(&mut res_message).await?;
-        Ok(())
-    }
-
-    pub async fn start_transfer(
-        &mut self,
-        wtr: &mut impl Write,
-        f: impl Fn(u64) -> (),
-    ) -> Result<(), Box<dyn Error>> {
-        let chunks = calc_chunks(self.size.unwrap());
-        let mut data = Vec::with_capacity(1_000_000);
-        for _ in 0..chunks {
-            self.ws_conn.recv(&mut data).await?;
-            let mut msg: ContentMessage = serde_json::from_slice(&data)?;
-            f(msg.content.len().try_into()?);
-            wtr.write(&mut msg.content)?;
-            data.clear();
-        }
-        self.ws_conn.close().await?;
-        Ok(())
-    }
+    self.ws_conn.close().await?;
+    Ok(())
+  }
 }
--- src/lib/utils.rs
+++ src/lib/utils.rs
@@ -12,64 +12,65 @@ const PUBLIC_RELAY: &str = "139.177.178.244:8004";
 
 // CONNECTIONS
 pub async fn start_ws_conn(
-    relay_addr: SocketAddr,
+  relay_addr: SocketAddr,
 ) -> Result<(SenderSk<Compat<TcpStream>>, ReceiverSk<Compat<TcpStream>>), Box<dyn Error>> {
-    let socket = TcpStream::connect(relay_addr).await?;
-    let str_addr = relay_addr.clone().to_string();
-    let mut client = Client::new(socket.compat(), &str_addr, "/");
+  let socket = TcpStream::connect(relay_addr).await?;
+  let str_addr = relay_addr.clone().to_string();
+  let mut client = Client::new(socket.compat(), &str_addr, "/");
 
-    let (sender, receiver) = match client.handshake().await? {
-        ServerResponse::Accepted { .. } => client.into_builder().finish(),
-        ServerResponse::Redirect { .. } => unimplemented!("f"),
-        ServerResponse::Rejected { .. } => unimplemented!("f"),
-    };
+  let (sender, receiver) = match client.handshake().await? {
+    ServerResponse::Accepted { .. } => client.into_builder().finish(),
+    ServerResponse::Redirect { .. } => unimplemented!("f"),
+    ServerResponse::Rejected { .. } => unimplemented!("f"),
+  };
 
-    Ok((sender, receiver))
+  Ok((sender, receiver))
 }
 
 pub async fn start_ws_handshake(
-    stream: TcpStream,
+  stream: TcpStream,
 ) -> Result<(SenderSk<Compat<TcpStream>>, ReceiverSk<Compat<TcpStream>>), Box<dyn Error>> {
-    let mut server = soketto::handshake::Server::new(stream.compat());
-    let websocket_key = {
-        let req = server.receive_request().await?;
-        req.key()
-    };
-    let accept = Response::Accept {
-        key: websocket_key,
-        protocol: None,
-    };
-    server.send_response(&accept).await?;
+  let mut server = soketto::handshake::Server::new(stream.compat());
+  let websocket_key = {
+    let req = server.receive_request().await?;
+    req.key()
+  };
+  let accept = Response::Accept {
+    key: websocket_key,
+    protocol: None,
+  };
+  server.send_response(&accept).await?;
 
-    let (sender, receiver) = server.into_builder().finish();
-    Ok((sender, receiver))
+  let (sender, receiver) = server.into_builder().finish();
+  Ok((sender, receiver))
 }
 
 // Rand
 pub fn gen_room_key() -> String {
-    let phrase = random_phrase(3);
-    str::replace(&phrase, " ", "-")
+  let phrase = random_phrase(3);
+  str::replace(&phrase, " ", "-")
 }
 
 // Format
 pub fn calc_chunks(size: u64) -> u64 {
-    ((size as f32 + 1_000_000.0 - 1.0) / 1_000_000.0) as u64
+  ((size as f32 + 1_000_000.0 - 1.0) / 1_000_000.0) as u64
 }
 
 // Validation
 pub fn parse_relay_addr(addr: Option<&str>) -> SocketAddr {
-    if let Some(addr) = addr {
-        let addr = addr.parse().expect("Couldn't parse the provided address");
-        addr
-    } else {
-        PUBLIC_RELAY.parse().unwrap()
-    }
+  if let Some(addr) = addr {
+    let addr = addr.parse().expect("Couldn't parse the provided address");
+    addr
+  } else {
+    PUBLIC_RELAY.parse().unwrap()
+  }
 }
+
 
 #[cfg(test)]
 mod tests {
+    use std::net::{SocketAddr, IpAddr, Ipv4Addr};
     use super::*;
-    use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
     #[test]
     fn test_gen_room_key() {
